336480a4c5132252cb9ec7eb873331da3b3f3ae4
FarisHijazi <a.fhijazi@dev.thiqah.sa>
2024-04-24 22:07:38 +0300

    added najizQA data and postprocess script

---
+++ scrapers/NajizQA/postprocess.py
@@ -0,0 +1,27 @@
+from bs4 import BeautifulSoup
+import re
+import json
+
+
+najizQA = json.load(open('./najiz_faq.json'))
+
+
+def get_innder_text_from_HTML_string(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    return soup.get_text()
+
+
+def remove_double_spaces(text):
+    return re.sub(' +', ' ', text.replace('\xa0', ' ')).strip()
+
+
+benchmark = [
+    {
+        'question': remove_double_spaces(get_innder_text_from_HTML_string(x['question'])),
+        'ground_truth': remove_double_spaces(get_innder_text_from_HTML_string(x['answer'])),
+    }
+    for x in najizQA
+]
+
+with open('benchmark.json', 'w') as f:
+    json.dump(benchmark, f, indent=4)
